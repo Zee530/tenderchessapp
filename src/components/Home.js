7d4c09cbf429d3ee4df5f23cdddc55b06cf77dbf
Zee530 <Zeee530@gmail.com>
2022-02-14 22:26:42 +0100

    safe

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
+import { useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
 import image from '../images/chess-2730034_1920.jpg'
+// import LoginModal from './LoginModal'
+import Modal from 'react-bootstrap/Modal'
 
 
 function Home() {
+    const [adminShow, setadminShow] = useState(false);
+    const [contestantShow, setcontestantShow] = useState(false)
     return (
     <>
+        {/* <LoginModal /> */}
         <Container>
         <div className="text-center p-3">
             <img src={image}
@@ -13,8 +19,36 @@ function Home() {
                 height='auto'
                 alt='chessboard'/>
         </div>
-            <Button className='button1' size='xxl'>ADMIN <br/>LOGIN</Button>  
-            <Button className='button2' size='xxl'>CONTESTANT <br/>LOGIN</Button> 
+            <Button className='button1' size='xxl' onClick={() => setadminShow(true)}>ADMIN <br/>LOGIN</Button>  
+            <Button className='button2' size='xxl' onClick={() => setcontestantShow(true)}>CONTESTANT <br/>LOGIN</Button> 
+
+            <Modal
+                show={adminShow}
+                onHide={() => setadminShow(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        ADMIN LOGIN
+                    </Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    Form
+                </Modal.Body>
+            </Modal>
+
+            <Modal
+                show={contestantShow}
+                onHide={() => setcontestantShow(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>
+                        CONTESTANT LOGIN
+                    </Modal.Title>
+                </Modal.Header>
+
+                <Modal.Body>
+                    Form
+                </Modal.Body>
+            </Modal>
         </Container>
     </>
     )
---
+++ src/components/LoginModal.js
@@ -0,0 +1,41 @@
+// import {useState} from 'react'
+// import Modal from 'react-bootstrap/Modal'
+
+// function LoginModal() {
+//     const [adminShow, setadminShow] = useState(false);
+//     const [contestantShow, setcontestantShow] = useState(false)
+
+//     return (
+//         <>
+//             <Modal
+//                 show={adminShow}
+//                 onHide={() => setadminShow(false)}>
+//                 <Modal.Header closeButton>
+//                     <Modal.Title>
+//                         ADMIN LOGIN
+//                     </Modal.Title>
+//                 </Modal.Header>
+
+//                 <Modal.Body>
+//                     Form
+//                 </Modal.Body>
+//             </Modal>
+
+//             <Modal
+//                 show={contestantShow}
+//                 onHide={() => setcontestantShow(false)}>
+//                 <Modal.Header closeButton>
+//                     <Modal.Title>
+//                         CONTESTANT LOGIN
+//                     </Modal.Title>
+//                 </Modal.Header>
+
+//                 <Modal.Body>
+//                     Form
+//                 </Modal.Body>
+//             </Modal>
+//         </>
+//     )
+// }
+
+// export default LoginModal
