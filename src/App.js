9fd6c576394e6e02481ec0a147bba3e657c36c19
Zee530 <Zeee530@gmail.com>
2022-02-25 22:04:55 +0100

    recent

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import { useState } from 'react'
 import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
-import image from './images/chess-2730034_1920.jpg'
+import chess1 from './images/chess1.jpg'
+import chess2 from './images/chess2.jpg'
+import chess3 from './images/chess3.jpg'
+import chess4 from './images/chess4.jpg'
+import chess5 from './images/chess5.png'
+import chess6 from './images/chess6.jpg'
+import chess7 from './images/chess7.jpg'
 // import LoginModal from './LoginModal'
 import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
@@ -18,11 +24,47 @@ function App() {
         {/* <LoginModal /> */}
         <Navbar1 />
         <Container>
-        <div className="text-center p-3">
-            <img src={image}
-                width='950px'
-                height='auto'
-                alt='chessboard'/>
+        <div id='chessSlide' class='carousel slide mt-3 mb-3' data-bs-ride='carousel'>
+            <div class='carousel-indicators'>
+                <button type='button' data-bs-target='#chessSlide' data-bs-slide-to='0' class='active' aria-current='true' aria-label='Slide 1'></button>
+                <button type='button' data-bs-target='#chessSlide' data-bs-slide-to='1' aria-label='Slide 2'></button>
+                <button type='button' data-bs-target='#chessSlide' data-bs-slide-to='2' aria-label='Slide 3'></button>
+                <button type='button' data-bs-target='#chessSlide' data-bs-slide-to='3' aria-label='Slide 4'></button>
+                <button type='button' data-bs-target='#chessSlide' data-bs-slide-to='4' aria-label='Slide 5'></button>
+                <button type='button' data-bs-target='#chessSlide' data-bs-slide-to='5' aria-label='Slide 6'></button>
+                <button type='button' data-bs-target='#chessSlide' data-bs-slide-to='6' aria-label='Slide 7'></button>
+            </div>
+            <div class='carousel-inner'>
+                <div class='carousel-item'>
+                    <img src={chess1} alt='c1' class='d-block w-100'/>
+                </div>
+                <div class='carousel-item'>
+                    <img src={chess2} alt='c2' class='d-block w-100'/>
+                </div>
+                <div class='carousel-item'>
+                    <img src={chess3} alt='c3' class='d-block w-100'/>
+                </div>
+                <div class='carousel-item active'>
+                    <img src={chess4} alt='c4' class='d-block w-100'/>
+                </div>
+                <div class='carousel-item'>
+                    <img src={chess5} alt='c5' class='d-block w-100'/>
+                </div>
+                <div class='carousel-item'>
+                    <img src={chess6} alt='c6' class='d-block w-100'/>
+                </div>
+                <div class='carousel-item'>
+                    <img src={chess7} alt='c7' class='d-block w-100'/>
+                </div>
+            </div>
+            <button class='carousel-control-prev' type='button' data-bs-target='#chessSlide' data-bs-slide='prev'>
+                <span class='carousel-control-prev-icon' aria-hidden='true'></span>
+                <span class='visually-hidden'>Previous</span>
+            </button>
+            <button class='carousel-control-next' type='button' data-bs-target='#chessSlide' data-bs-slide='next'>
+                <span class='carousel-control-next-icon' aria-hidden='true'></span>
+                <span class='visually-hidden'>Next</span>
+            </button>
         </div>
             <Button className='button1' size='xxl' onClick={() => setadminShow(true)}>ADMIN <br/>LOGIN</Button>  
             <Button className='button2' size='xxl' onClick={() => setcontestantShow(true)}>CONTESTANT <br/>LOGIN</Button> 
--- src/components/ContestHome.js
+++ src/components/ContestHome.js
@@ -1,25 +1,37 @@
 import Container from "react-bootstrap/Container"
 import Button from "react-bootstrap/Button"
-import image from '../images/chess-2730034_1920.jpg'
+import image from '../images/chess3.jpg'
 import { Outlet } from 'react-router-dom'
 import Navbar2 from './Navbar2'
 import { Link } from "react-router-dom";
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from "react-bootstrap/Card"
 
 function contestHome() {
     return (
         <>
-        <Navbar2 />
-        <Container>
-            <div className="text-center p-3">
-                <img src={image}
-                    width='950px'
-                    height='auto'
-                    alt='chessboard'/>
-            </div>
-            <Link to='/pool'><Button className='button1' size='xxl'>Start Tournament</Button></Link> 
-            <Link to='/score'><Button className='button2' size='xxl'>View Score/Rank</Button></Link> 
-        </Container>
-        <Outlet />
+        <Navbar2 />  
+        <Row>
+            <Col></Col>            
+            <Col md={9}>
+                <Card>
+                    <Card.Header style={{backgroundColor:'#e6ac00'}}><h3 className="text-center">CONTESTANT HOME</h3></Card.Header>
+                    <Card.Body>        
+                            <div className="text-center p-3">
+                                <img src={image}
+                                    width='950px'
+                                    height='auto'
+                                    alt='chessboard'/>
+                            </div>
+                            <Link to='/pool'><Button className='button1' size='xxl'>Start Tournament</Button></Link>
+                            <Link to='/score'><Button className='button2' size='xxl'>View Score</Button></Link>
+                        <Outlet />
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col></Col>
+        </Row>      
         </>
     )
 }
